ef44af25da293b061b1351c9fcb4a65b18a271d6
igiranezah59 <higiranez@gmail.com>
2024-06-12 11:14:47 +0200

    deliver affirmation app

---
+++ app/src/main/java/com/example/affrimationapp/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.affrimationapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.affrimationapp.data.DataResource
+import com.example.affrimationapp.ui.theme.AffrimationAppTheme
+import javax.sql.DataSource
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AffrimationAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    AffirmationApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+        AffirmationList(affirmationList = DataResource().loadAffirmations() )
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier){
+    Card(modifier = modifier) {
+        Column(Modifier.padding(0.dp,36.dp,0.dp,0.dp)) {
+            Image(
+                painter = painterResource(affirmation.imageResourceId),
+                contentDescription = stringResource(affirmation.stringResourceId),
+                modifier = modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>){
+    LazyColumn(modifier = Modifier) {
+        items(affirmationList){affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+dd
+@Preview(showBackground = true)
+@Composable
+fun AffirmationAppPreview() {
+    AffrimationAppTheme {
+        AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1), modifier = Modifier)
+    }
+}
